1829de32f32fe83ca4225837de2885782012cf13
Alistair Cunningham <acunningham@integrics.com>
2026-01-12 11:27:57 +0800

    Add NotificationTitle component to root

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,9 +1,10 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { NavigationProgress, Toaster } from '@mochi/common'
+import { NavigationProgress, NotificationTitle, Toaster } from '@mochi/common'
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <NotificationTitle />
       <NavigationProgress />
       <Outlet />
       <Toaster duration={5000} />
