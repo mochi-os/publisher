b1bea7266df6f09d22c479925db20a3e13361e90
Alistair Cunningham <acunningham@integrics.com>
2026-05-02 09:15:36 +0200

    Remove pseudo-locale wiring. en-x-pseudo dropped from lingui.config.js, the matching catalog import dropped from main.tsx, and the auto-generated src/locales/en-x-pseudo/ directory removed. Won't be used in practice.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -15,7 +15,6 @@ const catalogs: Catalogs = {
   ja: () => import('./locales/ja/messages.po'),
 
   ar: () => import('./locales/ar/messages.po'),
-  'en-x-pseudo': () => import('./locales/en-x-pseudo/messages.po'),
 }
 
 const queryClient = createQueryClient()
